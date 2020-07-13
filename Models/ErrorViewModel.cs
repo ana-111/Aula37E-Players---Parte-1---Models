c2acb01dc6e57b4595e9befa288841bf50fa73d8
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-07-13 10:09:48 -0300

    Peojeto iniciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aula37E_Players___Parte_1___Models.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
